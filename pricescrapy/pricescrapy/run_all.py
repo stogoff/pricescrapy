3c8246d7544864eb13020acb4e141768b1516a17
Rost <rost@stogoff.ru>
2019-08-26 22:08:33 +0300

    test

--- pricescrapy/pricescrapy/run_all.py
+++ pricescrapy/pricescrapy/run_all.py
@@ -10,14 +10,16 @@ import os.path
 #from spiders.masterglass import MasterglassSpider
 
 
-t1 = os.path.getmtime('/tmp/uploads/input.txt')
-t2 = os.path.getmtime('static/result.csv')
-if t2>t1:
-    print('old')
-    sys.exit()
-file = open('static/result.csv', 'w')
-file.close()
+#t1 = os.path.getmtime('/tmp/uploads/input.txt')
+#t2 = os.path.getmtime('static/result.csv')
+#if t2>t1:
+#    print('old')
+#    sys.exit()
+#file = open('static/result.csv', 'w')
+#file.close()
 process = CrawlerProcess(get_project_settings())
+print(process.settings['INPUT_FILENAME'][-15:-4])
+sys.exit()
 process.crawl('mirposudy')
 process.crawl('posuda-pro')
 process.crawl('projecthotel')
