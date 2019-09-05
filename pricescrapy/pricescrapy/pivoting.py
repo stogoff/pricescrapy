894f0bc5342688c618eaac143b467dee802e3470
Rost <rost@stogoff.ru>
2019-09-05 15:59:54 +0300

    pivot table

---
+++ pricescrapy/pricescrapy/pivoting.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def return_hyperlink(x):
+    return '=HYPERLINK("{0}", {1})'.format(x['link'], x['price'])
+
+
+df = pd.read_csv('static/result1566972765.csv', delimiter=';', header=None,
+                 names=['art', 'title', 'price', 'shop', 'link'])
+df['linkedprice'] = df.apply(return_hyperlink, axis=1)
+
+dfp = pd.pivot_table(df, values=['linkedprice'], index='art', columns='shop', aggfunc='first')
+
+print(dfp.iloc[0, 3])
+
+dfp.to_excel("output.xlsx")
