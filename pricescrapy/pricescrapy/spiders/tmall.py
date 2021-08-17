5059d95f061e3a9aac6af5fd074f5483c0e3450a
Rostislav Stogov <rost@stogoff.ru>
2021-08-17 14:35:12 +0300

    new spiders

--- pricescrapy/pricescrapy/spiders/tmall.py
+++ pricescrapy/pricescrapy/spiders/tmall.py
@@ -30,22 +30,35 @@ class TmallSpider(scrapy.Spider):
                                       meta={'art': art, 'brand': brand},
                                       wait_time=5,
                                       screenshot=False,
-                                      callback=self.parse)
+                                      callback=self.parse_search)
 
-    def parse(self, response):
+    def parse_search(self, response):
         art = response.meta['art']
         brand = response.meta['brand']
         try:
-            price = response.css('div.price').css('span::text').get().replace('\xa0', '')
-            price = re.split(r'\W+', price)[0]
+            # price = response.css('div.price').css('span::text').get().replace('\xa0', '')
+            # price = re.split(r'\W+', price)[0]
             title = ''.join(response.css('a.product').xpath('.//text()').getall())
             title = title.replace("\n", " ").replace("\t", "").strip()
             link = 'https://aliexpress.ru' + response.css('a.product::attr(href)').get().split('?')[0]
-            yield {'title': title,
-                   'link': link,
-                   'price': price,
-                   'shop': self.name,
-                   'art': art
-                   }
-        except:
-            pass
+            yield SeleniumRequest(url=link,
+                                  dont_filter=True,
+                                  headers={'Referer': response.url},
+                                  meta={'art': art, 'brand': brand, 'title': title},
+                                  wait_time=5,
+                                  screenshot=False,
+                                  callback=self.parse_item)
+        except AttributeError:
+            self.logger.info('NOT FOUND{}'.format(art))
+
+    def parse_item(self, response):
+        art = response.meta['art']
+        title = response.meta['title']
+        price = response.css('span.product-price-current::text').get().replace('\xa0', '')
+        price = re.split(r'\W+', price)[0]
+        yield {'title': title,
+               'link': response.url,
+               'price': price,
+               'shop': self.name,
+               'art': art
+               }
