9059283e306d4599c6d464816cca81ff34521892
Rostislav Stogov <rost@stogoff.ru>
2021-10-15 12:44:53 +0300

    status line

--- pricescrapy/pricescrapy/status.py
+++ pricescrapy/pricescrapy/status.py
@@ -1,25 +1,31 @@
 import os
 import time
+
+
 def status():
     in_path = '/tmp/uploads'
-    files=[]
+    files = []
     for r, d, f in os.walk(in_path):
         for filename in f:
             if '.txt' in filename:
                 files.append(filename)
+    if not files:
+        return ""
     files.sort()
     txt = files[-1]
     out_path = 'static/'
-    files=[]
+    files = []
     for r, d, f in os.walk(out_path):
         for filename in f:
             if '.csv' in filename:
                 files.append(filename)
+    if not files:
+        return f'Файл {txt} ожидает обработки'
     files.sort()
     csv = files[-1]
     print(csv)
     timestamp = int(csv[-14:-4])
-    xlsx = csv[:-3]+'xlsx'
+    xlsx = csv[:-3] + 'xlsx'
     print(xlsx)
     link = '/static/result{}.xlsx'.format(timestamp)
     xlink = '/static/result_p{}.xlsx'.format(timestamp)
@@ -29,5 +35,5 @@ def status():
     else:
         t = int(time.time() - timestamp)
         status = f'Файл {txt} обрабатывается {t} c. По окончании результаты будут доступны  <a href="{link}">здесь</a> и <a href="{xlink}">здесь</a>'
-    print (status)
-    return  status
+    print(status)
+    return status
