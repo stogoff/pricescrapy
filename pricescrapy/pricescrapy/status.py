36e5c80399c9f98dad8e961663b4ebaa11c2ddfb
Rostislav Stogov <rost@stogoff.ru>
2021-10-15 14:10:32 +0300

    status line

--- pricescrapy/pricescrapy/status.py
+++ pricescrapy/pricescrapy/status.py
@@ -1,8 +1,15 @@
 import os
 import time
+import psutil
 
 
 def status():
+    running = False
+    for pid in psutil.pids():
+        process = psutil.Process(pid)
+        if 'python' in process.name():
+            if 'run_all.py' in process.cmdline()[1]:
+                running = True
     in_path = '/tmp/uploads'
     xfiles = []
     xtimestamp = 0
@@ -24,7 +31,7 @@ def status():
     files.sort()
     txt = files[-1]
     ttimestamp = int(txt[2:12])
-    if xtimestamp>ttimestamp:
+    if xtimestamp > ttimestamp:
         txt = inxls
     out_path = 'static/'
     files = []
@@ -40,20 +47,27 @@ def status():
     timestamp = int(csv[-14:-4])
     if timestamp < ttimestamp:
         return f'Файл {txt} ожидает обработки'
-    elif timestamp<xtimestamp:
+    elif timestamp < xtimestamp:
         return f'Файл {inxls} ожидает обработки'
     xlsx = csv[:-3] + 'xlsx'
     print(xlsx)
     link = '/static/result{}.xlsx'.format(timestamp)
     xlink = '/static/result_p{}.xlsx'.format(timestamp)
     if os.path.isfile(os.path.join(out_path, xlsx)):
-        size_csv =  os.path.getsize(os.path.join(out_path, csv))
+        size_csv = os.path.getsize(os.path.join(out_path, csv))
         if size_csv < 10:
             status = f'Произошла ошибка обработки файла {txt}. Обратитесь к разработчику'
         else:
             status = f'Файл обработан. Результаты доступны  <a href="{link}">здесь</a> и <a href="{xlink}">здесь</a>'
     else:
-        t = int(time.time() - timestamp)
-        status = f'Файл {txt} обрабатывается {t} c. По окончании результаты будут доступны  <a href="{link}">здесь</a> и <a href="{xlink}">здесь</a>'
+        if running:
+            t = int(time.time() - timestamp)
+            status = f'Файл {txt} обрабатывается {t} c. По окончании результаты будут доступны  <a href="{link}">здесь</a> и <a href="{xlink}">здесь</a>'
+        else:
+            status = f'Произошла ошибка обработки файла {txt}. Обратитесь к разработчику'
     print(status)
     return status
+
+
+if __name__ == "__main__":
+    status()
